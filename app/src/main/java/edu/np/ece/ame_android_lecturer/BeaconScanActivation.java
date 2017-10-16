6fda1ce13f2fe14fe5e56ca00e51ee62dbbaa207
Hubert <hubertdyz@gmail.com>
2017-10-16 14:50:36 +0800

    beacon base

---
+++ app/src/main/java/edu/np/ece/ame_android_lecturer/BeaconScanActivation.java
@@ -0,0 +1,19 @@
+package edu.np.ece.ame_android_lecturer;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+/**
+ * Created by dellpc on 10/16/2017.
+ */
+
+public class BeaconScanActivation {
+
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private BeaconManager mBeaconManager;
+
+    
+
+}
